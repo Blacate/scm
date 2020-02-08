bfb8be962f28fb061d7574fc526a2a70cc1ceaa7
Neail <blacated@gmail.com>
2020-02-08 18:14:24 +0800

    update deleteCommand

--- src/commands/delete_command.ts
+++ src/commands/delete_command.ts
@@ -1,6 +1,8 @@
 import * as yargs from 'yargs';
 import { SshClientService } from '../features/ssh_client/ssh_client.service';
 import { initConnection } from '../utils/handle_connection';
+import { printItem } from '../utils/print';
+import * as prompts from 'prompts';
 
 export class DeleteCommand implements yargs.CommandModule {
   private sshClientService: SshClientService;
@@ -21,8 +23,16 @@ export class DeleteCommand implements yargs.CommandModule {
   async handler(args: yargs.Arguments) {
     const result = await this.sshClientService.getByAlias(args.alias as string);
     if (result) {
-      // todo: ensure
-      await this.sshClientService.deleteByAlias(args.alias as string);
+      printItem(result);
+      const { value } = await prompts({
+        type: 'confirm',
+        name: 'value',
+        message: 'Delete it?',
+        initial: false,
+      });
+      if (value) {
+        await this.sshClientService.deleteByAlias(args.alias as string);
+      }
     } else {
       console.log(`Alias: ${args.alias} is not exist!`);
     }
