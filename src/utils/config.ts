b9e66241dae3cdb5ae2c337c52b92ebdbf858184
Neail <blacated@gmail.com>
2020-02-08 19:53:41 +0800

    fix default config bug

---
+++ src/utils/config.ts
@@ -0,0 +1,55 @@
+import { ConnectionOptions } from 'typeorm';
+import { homedir } from 'os';
+import { join } from 'path';
+import { existsSync, writeFileSync } from 'fs';
+
+let config: {
+  db: ConnectionOptions,
+}
+
+const defaultConfig = `{
+  "db": {
+    "type": "sqlite",
+    "database": "~/.ssh/scm.sqlite",
+    "entities": ["dist/**/**.entity{.ts,.js}"],
+    "synchronize": true
+  },
+  "mysqlDB": {
+    "type": "mysql",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "username": "root",
+    "password": "1111",
+    "database": "scm",
+    "entities": ["dist/**/**.entity{.ts,.js}"],
+    "synchronize": true
+  }
+}
+`
+
+const getConfig = () => {
+  if (config) return config;
+
+  const configPath = join(homedir(), '.ssh/scm_sql_application.json')
+
+  if (!existsSync(configPath)) {
+    writeFileSync(configPath, defaultConfig, 'utf8')
+  }
+
+  // tslint:disable-next-line: no-var-requires
+  const loadedConfig = require(configPath);
+
+  // 修复sqlite中homedir为～的问题
+  if (loadedConfig.db.type === 'sqlite') {
+    loadedConfig.db.database = loadedConfig.db.database.replace(/^~/, homedir())
+  }
+
+  // 解决connectionOptions中属性为readonly问题
+  config = loadedConfig;
+  return config;
+
+}
+
+export {
+  getConfig
+}
--- src/utils/handle_connection.ts
+++ src/utils/handle_connection.ts
@@ -1,20 +1,8 @@
 import { createConnection, getConnection, ConnectionOptions } from 'typeorm';
-import { homedir } from 'os';
-import { join } from 'path';
-
-// todo handle config
-
-// tslint:disable-next-line: no-var-requires
-const config = require(join(homedir(), '.ssh/node_scm.json'));
-
-const connectionOpions = config.db
-
-if (connectionOpions.type === 'sqlite') {
-  connectionOpions.database = connectionOpions.database.replace(/^~/, homedir())
-}
+import { getConfig } from './config';
 
 const initConnection = async () => {
-  await createConnection(connectionOpions).catch(err => {
+  await createConnection(getConfig().db).catch(err => {
     console.log(err);
     process.exit();
   });
